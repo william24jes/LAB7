26dc12f1c7dea28b3db3a2429e8238ca0a56a99a
william <william24jes@hotmail.com>
2023-05-30 18:29:47 -0500

    unversioned files

---
+++ LAB7-20182895/src/main/java/com/example/lab720182895/Lab720182895Application.java
@@ -0,0 +1,13 @@
+package com.example.lab720182895;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab720182895Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab720182895Application.class, args);
+    }
+
+}
---
+++ LAB7-20182895/src/test/java/com/example/lab720182895/Lab720182895ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab720182895;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab720182895ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
